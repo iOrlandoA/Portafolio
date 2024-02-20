8d678f0c445443ee99e66f68e1a278bb7e6df33b
iOrlandoA <orlando.aguilar@ucrso.info>
2024-02-19 23:56:48 -0600

    Added new Skills

--- darkMode.js
+++ darkMode.js
@@ -5,12 +5,15 @@ const diaCardText = "#303030";
 const diaCardBack = "#F4F4F4";
 const diaBodyText = "#0D0C08";
 const diaBodyBack = "#EEF4ED";
+const diaBackgroundGradiente= "rgb(255, 255, 255)";
 
 
 const nocheCardText = "#F4F4F4";
 const nocheCardBack = "#303030";
 const nocheBodyText = "#F4F4F4";
 const nocheBodyBack = "#121715";
+const nocheBackgroundGradiente= "rgba(255, 255, 255, 0.1)";
+
 
 var darkMode = true;
 
@@ -25,12 +28,14 @@ const switchDark=(darkMode)=>{
     root.style.setProperty('--color-cardBackGround', nocheCardBack);
     root.style.setProperty('--color-bodyText', nocheBodyText);
     root.style.setProperty('--color-bodyBackGround', nocheBodyBack);
+    root.style.setProperty('--background-gradiente', nocheBackgroundGradiente);
   }else{
     localStorage.setItem('darkMode', false);
     root.style.setProperty('--color-cardTexto', diaCardText);
     root.style.setProperty('--color-cardBackGround', diaCardBack);
     root.style.setProperty('--color-bodyText', diaBodyText);
     root.style.setProperty('--color-bodyBackGround', diaBodyBack);
+    root.style.setProperty('--background-gradiente', diaBackgroundGradiente);
   }
   this.darkMode= darkMode;
 }
